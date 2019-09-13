27a884a06eefb96c835dbe8063d1d5fe914ce200
frogcat <frogcat@users.noreply.github.com>
2019-09-13 12:04:46 +0900

    Update data, fix data timestamp, add data list

--- data.sh
+++ data.sh
@@ -2,8 +2,12 @@
 
 code=$1
 
-now=`date +%Y%m%d%H%m`
+now=`date +%Y%m%d%H%M`
 html=${code}000000000.html
+
+echo $now
 
 node main.js ${html} > docs/data.${now}.json
 cp docs/data.${now}.json docs/data.json
+
+find . -name 'data.*.json' | sed -ne 's#./docs/##p' > docs/data.txt
